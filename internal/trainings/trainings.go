a34cc14c5b4aa7b9764527024c334b4985bf1c6a
Valeria <luts.valeriya@bk.ru>
2025-12-06 17:25:58 +0500

    5sprint

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -16,7 +16,7 @@ type Training struct {
 	Steps        int
 	TrainingType string
 	Duration     time.Duration
-	Personal     personaldata.Personal
+	personaldata.Personal
 }
 
 func (t *Training) Parse(datastring string) (err error) {
