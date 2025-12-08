3ffae0f8c84c853cf0a796d5bd3354874f6b3002
Valeria <luts.valeriya@bk.ru>
2025-12-08 19:51:25 +0500

    5sprint

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -23,14 +23,14 @@ func (ds *DaySteps) Parse(datastring string) (err error) {
 	parts := strings.Split(datastring, ",")
 
 	if len(parts) != 2 {
-		return fmt.Errorf("data parse error: expected 3 parts (steps,name,duration), got %d", len(parts))
+		return fmt.Errorf("data parse error: expected 2 parts (steps,name,duration), got %d", len(parts))
 	}
 
 	var steps int
 	steps, err = strconv.Atoi(parts[0])
 	if err != nil {
 		err = fmt.Errorf("failed to parse steps '%s': %w", parts[0], err)
-		return
+		return err
 	}
 	if steps <= 0 {
 		err = errors.New("the number of steps is less than or equal to 0")
@@ -40,7 +40,7 @@ func (ds *DaySteps) Parse(datastring string) (err error) {
 	ds.Steps = steps
 
 	var dur time.Duration
-	dur, err = time.ParseDuration(parts[2])
+	dur, err = time.ParseDuration(parts[1])
 	if err != nil {
 		return fmt.Errorf("failed to parse duration '%s': %w", parts[2], err)
 	}
